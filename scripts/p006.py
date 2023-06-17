53ea8e96f86ac4e91beb460eca7edb3f87cb219b
Hoshmand M. Qadir <yt.hoshmand@gmail.com>
2023-06-17 12:33:29 +0300

    Update Copyright

--- scripts/p006.py
+++ scripts/p006.py
@@ -1,3 +1,8 @@
+# Copyright (c) Hoshmand. All rights reserved.
+# 
+# https://twitter.com/hoshmandctf
+# https://github.com/hoshmandctf
+
 # Calculate the sum of squares
 sum_of_squares = sum(i**2 for i in range(1, 101))
 
